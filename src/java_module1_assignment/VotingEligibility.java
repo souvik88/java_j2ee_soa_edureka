e2782f5db14c0594cc92062196c58be6dd63f167
Souvik Kundu <kivtech88@gmail.com>
2020-11-10 15:31:28 +0530

    commit

--- src/java_module1_assignment/VotingEligibility.java
+++ src/java_module1_assignment/VotingEligibility.java
@@ -1,17 +1,20 @@
 package java_module1_assignment;
-
+import java.util.Scanner;
 public class VotingEligibility {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Write a program to check if a candidate is eligible for voting or not
-		int age = 61;
-		if(age >= 18 && age >=60) {
+		Scanner scanUser = new Scanner(System.in);
+		System.out.println("What is your age? ");
+		int userAgeInput = scanUser.nextInt();
+		if(userAgeInput >= 18 && userAgeInput >=60) {
 			System.out.println("You are a senior citizen and you are eligible to vote");
-		} else if (age >= 18) {
+		} else if (userAgeInput >= 18) {
 			System.out.println("You are eligible to vote");
 		} else {
 			System.out.println("You are not eligible to vote, you have to wait until 18th birthday");
 		}
+		scanUser.close();
 	}
 }
