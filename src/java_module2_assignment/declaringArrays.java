6510ffc8ed5a6998c509731ea0f2b14f37c0d500
Souvik Kundu <kivtech88@gmail.com>
2021-01-19 22:23:14 +0530

    Update declaringArrays.java

--- src/java_module2_assignment/declaringArrays.java
+++ src/java_module2_assignment/declaringArrays.java
@@ -3,8 +3,18 @@ package java_module2_assignment;
 public class declaringArrays {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// way 1
+		int arr1[] = new int[5]; // array length is 5
+		
+		// way 2
+		int arr3[] = {10, 20, 30, 40, 50};
+		
+		// way 4
+		int arr4[] = new int[] {10, 20, 30, 40, 50};
+		
+		char charArr[] = new char[10];
+		char charArr2[] = {'A', 'B', 'C', 'D'};
+		
 	}
 
 }
